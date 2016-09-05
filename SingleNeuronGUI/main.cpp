5ecd8362a37f0e8a2f91ce9c48fb5289157783e0
Sankarsan Seal <sankarsanseal@icloud.com>
2016-09-05 11:36:48 +0530

    Initial Commit

---
+++ SingleNeuronGUI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SingleNeuronGUI
+//
+//  Created by Sankarsan Seal on 05/09/16.
+//  Copyright © 2016 Sankarsan Seal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
